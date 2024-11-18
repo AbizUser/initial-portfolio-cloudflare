9d580cbc559dbdd668dfed4bb6ada047123f76ad
AbizUser <aman.biz.t23@gmail.com>
2024-11-18 15:35:08 +0900

    fix eslint define error

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
 
 const Skills = () => {
   return (
